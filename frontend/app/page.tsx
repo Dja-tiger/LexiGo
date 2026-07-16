dfe05a1433793441934c9f66b2502a30048abeb7
Dja_tiger <117261349+Dja-tiger@users.noreply.github.com>
2026-07-16 14:14:07 +0300

    feat: render resumable lesson app

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
-import { LexigoProductApp } from "../components/lexigo-product-app";
+import { LexigoResumableApp } from "../components/lexigo-resumable-app";
 
 export default function Home() {
-  return <LexigoProductApp />;
+  return <LexigoResumableApp />;
 }
