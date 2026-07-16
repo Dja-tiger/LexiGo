b507eb52892a8b6c06a82ca4c2d39c8187741370
Dja_tiger <117261349+Dja-tiger@users.noreply.github.com>
2026-07-16 14:14:30 +0300

    chore: refresh PWA shell for resumable lessons

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lexigo-shell-v4";
+const CACHE = "lexigo-shell-v5";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
