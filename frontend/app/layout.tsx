9c62d85e9e51f2cb627eaed65deda626ddd11c5d
Dja_tiger <117261349+Dja-tiger@users.noreply.github.com>
2026-07-20 00:22:20 +0300

    style(routes): register route boundary styles

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -13,6 +13,7 @@ import "./accessibility-focus.css";
 import "./accessibility-navigation.css";
 import "./adaptive-navigation.css";
 import "./route-navigation.css";
+import "./route-boundaries.css";
 import "./speech-player.css";
 import "./service-worker-update.css";
 import "./review-outbox.css";
